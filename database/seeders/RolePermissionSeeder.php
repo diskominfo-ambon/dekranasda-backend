14ddf230062f403c2654ede2fc8405080011b9d7
azmanabdlh <azmanabdlh@yahoo.com>
2021-11-05 12:52:56 +0900

    menambahkan role pengguna

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $roles = [
+            ['name' => 'user'],
+            ['name' => 'admin']
+        ];
+        $permissions = [];
+
+        Role::insert($roles);
+    }
+}
