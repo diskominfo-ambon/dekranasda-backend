c5d1a51f347d2e7271126fa81459ffbb0d3e8495
Azman Abdullah <azmanabdlh@yahoo.com>
2021-11-09 22:25:32 +0900

    menambahkan lib [Faker] ke mode produksi

--- app/Http/Controllers/Api/PostsController.php
+++ app/Http/Controllers/Api/PostsController.php
@@ -2,16 +2,19 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
-use App\Http\Resources\PostCollection;
-use App\Http\Resources\PostResource;
 use App\Models\Post;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
+use App\Http\Resources\PostCollection;
+use Illuminate\Support\Facades\Request;
 
 class PostsController extends Controller
 {
     public function index()
     {
-        $posts = Post::latest()->limit(20)->get();
+        $posts = Post::latest()
+            ->limit(20)
+            ->get();
 
         return new PostCollection($posts);
     }
